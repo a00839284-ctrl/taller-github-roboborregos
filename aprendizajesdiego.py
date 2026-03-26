965479334c567b113abeb9663045321434924955
Diego Quevedo <A00839284@tec.mx>
2026-03-26 17:31:48 -0600

    AGREGAR APRENDIZAJES

---
+++ aprendizajesdiego.py
@@ -0,0 +1,3 @@
+
+print("Aprendí como puedo usar aGitHub como historial de versiones de mis poryectos en código" \
+"y que tmabién lo puedo usar un poco en la parte de proyect Management en el workflow de mis proyectos")
